6499c589067267898d19d1aa8dbe3d47599c0833
Janne <peterdev80@gmail.com>
2020-02-24 13:34:12 +0200

    Updates

---
+++ precache-manifest.8f32f002f57f3c0cf9993cbc242df43d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb4f8947519a5bfb0e3699138f4bc3c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bbcc891114cbe2341407",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5a5dd13fb5e1f5eeb377",
+    "url": "/portfolio/static/js/2.f175dcd4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.f175dcd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbcc891114cbe2341407",
+    "url": "/portfolio/static/js/main.75ce4147.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
---
+++ static/js/main.75ce4147.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{3:function(n,e,t){n.exports=t(9)},8:function(n,e,t){},9:function(n,e,t){"use strict";t.r(e);var o=t(0),r=t.n(o),l=t(2),a=t.n(l);t(8);var c=function(){return r.a.createElement("div",null,r.a.createElement("h1",null,"Coming soon!"))};a.a.render(r.a.createElement(c,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.75ce4147.chunk.js.map
